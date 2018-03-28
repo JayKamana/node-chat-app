ea934f293d6aad7e5f996a8ab03abe10aa5b0be9
James Kamana <jaykamana@gmail.com>
2018-03-28 22:21:18 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+let publicPath = path.join(__dirname, '../public');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
+
